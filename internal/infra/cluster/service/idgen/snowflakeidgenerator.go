a614bd7ce9e490bfb0870ac8105c15c4390dff6f
Nils <kortnernils@gmail.com>
2025-01-23 19:16:09 +0100

    finished snowflakeidgenerator

--- internal/infra/cluster/service/idgen/snowflakeidgenerator.go
+++ internal/infra/cluster/service/idgen/snowflakeidgenerator.go
@@ -2,7 +2,9 @@ package idgen
 
 import (
 	"fmt"
+	"math"
 	"sync/atomic"
+	"time"
 )
 
 const (
@@ -28,26 +30,70 @@ type SnowflakeIdGenerator struct {
 	workerId       int64
 }
 
-func NewSnowflakeIdGenerator(dataCenterId, workerId int) *SnowflakeIdGenerator {
+func NewSnowflakeIdGenerator(dataCenterId, workerId int) (*SnowflakeIdGenerator, error) {
 	generator := &SnowflakeIdGenerator{}
-	generator.updateNodeInfo(dataCenterId, workerId)
-	return generator
+	err := generator.updateNodeInfo(dataCenterId, workerId)
+	if err != nil {
+		return generator, err
+	}
+	return generator, nil
 }
 
-// TODO: panic?
-func (s *SnowflakeIdGenerator) updateNodeInfo(dataCenterId, workerId int) {
+func (s *SnowflakeIdGenerator) updateNodeInfo(dataCenterId, workerId int) error {
 	if dataCenterId >= MAX_DATA_CENTER_ID {
 		reason := fmt.Sprintf("the data center ID must be in the range: [0, %d], but got: %d", MAX_DATA_CENTER_ID, dataCenterId)
-		panic(reason)
+		return fmt.Errorf(reason)
 	}
 	if workerId >= (1 << WORKER_ID_BITS) {
 		reason := fmt.Sprintf("the worker ID must be in the range: [0, %d], but got: %d", 1<<WORKER_ID_BITS, workerId)
-		panic(reason)
+		return fmt.Errorf(reason)
 	}
 	s.dataCenterId = int64(dataCenterId)
 	s.workerId = int64(workerId)
+	return nil
 }
 
 func (s *SnowflakeIdGenerator) nextIncreasingId() int64 {
-	sequenceNum := s.sequenceNumber.
+	// prepare each ID part
+	sequenceNum := s.sequenceNumber.Add(1) & SEQUENCE_NUMBER_MASK
+	updateAndGet := func() int64 {
+		for {
+			stamp := s.lastTimestamp.Load()
+			nonBackwardsTimestamp := int64(math.Max(float64(stamp), float64(time.Now().UnixMilli())))
+			if sequenceNum == 0 {
+				nonBackwardsTimestamp++
+			}
+			if s.lastTimestamp.CompareAndSwap(stamp, nonBackwardsTimestamp) {
+				return nonBackwardsTimestamp
+			}
+		}
+	}
+	timestamp := updateAndGet() - EPOCH
+
+	// Get ID
+	return (timestamp << TIMESTAMP_LEFT_SHIFT) | (s.dataCenterId << DATA_CENTER_ID_SHIFT) |
+		(s.workerId << WORKER_ID_SHIFT) | sequenceNum
+}
+
+func (s *SnowflakeIdGenerator) nextLargeGapId() int64 {
+	// prepare each ID part
+	sequenceNum := s.sequenceNumber.Add(1) & SEQUENCE_NUMBER_MASK
+	updateAndGet := func() int64 {
+		for {
+			stamp := s.lastTimestamp.Load()
+			nonBackwardsTimestamp := int64(math.Max(float64(stamp), float64(time.Now().UnixMilli())))
+			if sequenceNum == 0 {
+				nonBackwardsTimestamp++
+			}
+			if s.lastTimestamp.CompareAndSwap(stamp, nonBackwardsTimestamp) {
+				return nonBackwardsTimestamp
+			}
+		}
+	}
+	timestamp := updateAndGet() - EPOCH
+
+	// Get ID
+	return (sequenceNum << (int64(TIMESTAMP_BITS + DATA_CENTER_ID_BITS + WORKER_ID_BITS))) |
+		(timestamp << (int64(DATA_CENTER_ID_BITS + WORKER_ID_BITS))) |
+		s.dataCenterId<<int64(WORKER_ID_BITS) | s.workerId
 }
--- internal/infra/cluster/service/idservice.go
+++ internal/infra/cluster/service/idservice.go
@@ -1,13 +1,14 @@
 package service
 
 import (
+	"gurms/internal/infra/cluster/service/idgen"
 	"gurms/internal/infra/logging/core/factory"
 	"gurms/internal/infra/logging/core/logger"
 )
 
 var ISERVICELOGGER logger.Logger = factory.GetLogger("IdService")
 
-const FLAKE_ID_GENERATORS_LENGTH = getSharedEnumConstants()
+const FLAKE_ID_GENERATORS_LENGTH = int(idgen.NumServiceTypes)
 
 type IdService struct {
 	discoveryService          *DiscoveryService
@@ -17,13 +18,31 @@ type IdService struct {
 }
 
 func NewIdService(discoveryService *DiscoveryService) *IdService {
-
-	idGenerators := make()
-	for i := 0; i < FLAKE_ID_GENERATORS_LENGTH; i++ {
-		id
-	}
-
-	return &IdService{
+	idService := &IdService{
 		discoveryService: discoveryService,
 	}
+
+	idGenerators := make([]*idgen.SnowflakeIdGenerator, FLAKE_ID_GENERATORS_LENGTH)
+	for i := 0; i < FLAKE_ID_GENERATORS_LENGTH; i++ {
+		generator, err := idgen.NewSnowflakeIdGenerator(0, 0)
+		if err != nil {
+			ISERVICELOGGER.FatalWithError("could not initialize SnowflakeIdGenerator", err)
+			continue
+		}
+		idGenerators[i] = generator
+	}
+	idService.idGenerators = idGenerators
+	discoveryService.addOnMembersChangeListener(func() {
+		dataCenterId := idService.findNewDataCenterId()
+	})
+
+	return idService
 }
+
+func (i *IdService) nextIncreasingId() int64 {}
+
+func (i *IdService) nextLargeGapId() int64 {}
+
+func (i *IdService) findNewDataCenterId() int {}
+
+func (i *IdService) findNewWorkerID() int {}
